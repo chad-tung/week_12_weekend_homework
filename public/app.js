84c2676a2eeb6b22d3b046f3b653341c60f7a9c7
chad-tung <chad.tung@gmail.com>
2017-12-12 01:42:10 +0000

    Added in compare pokemon, and also a bar chart

--- public/app.js
+++ public/app.js
@@ -7,9 +7,10 @@
 // Should have structured the table better. Would allow for easier styling. Don't forget that tables have thead and tbody, so it would have been more efficient to have cleared the tbody.
 
 var app = function() {
-  var url = "https://pokeapi.co/api/v2/pokemon/?limit=151"
+  var url = "https://pokeapi.co/api/v2/pokemon/?limit=802"
   makeRequest(url, requestComplete);
 };
+
 
 var makeRequest = function(url, callback) {
   var request = new XMLHttpRequest();
@@ -33,6 +34,13 @@ var requestData = function() {
   var pokemonData = JSON.parse(jsonString);
   console.log(pokemonData);
   populateData(pokemonData);
+};
+
+var compareData = function() {
+  if(this.status != 200) return;
+  var jsonString = this.responseText;
+  var pokemonData = JSON.parse(jsonString);
+  populateCompareData(pokemonData);
 }
 
 var capitalize = function(string) {
@@ -41,19 +49,39 @@ var capitalize = function(string) {
 
 var populateSelect = function(pokemonList) {
   var select = document.getElementById('pokemon-select');
+  var selectCompare = document.getElementById('compare-pokemon-select');
   pokemonList.forEach(function(pokemon) {
     var option = document.createElement('option');
     option.innerText = capitalize(pokemon.name);
     select.appendChild(option);
+
+    var option2 = document.createElement('option');
+    option2.innerText = capitalize(pokemon.name);
+    selectCompare.appendChild(option2);
   });
 
   selectChanged(select.selectedIndex, pokemonList);
 
+  compareChanged(selectCompare.selectedIndex, pokemonList);
+
+  compareBarChart();
+
   select.addEventListener('change', function() {
     selectChanged(select.selectedIndex, pokemonList)
-  })
+    compareBarChart();
+  });
+
+  selectCompare.addEventListener('change', function() {
+    compareChanged(selectCompare.selectedIndex, pokemonList);
+    compareBarChart();
+  });
 
 };
+
+var compareChanged = function(index, pokemonList) {
+  var url = pokemonList[index].url;
+  makeRequest(url, compareData);
+}
 
 var selectChanged = function(index, pokemonList) {
   var url = pokemonList[index].url;
@@ -89,10 +117,41 @@ var populateData = function(pokemonData) {
   pokemonStats(pokemonData);
 };
 
+var populateCompareData = function(pokemonData) {
+  var name = document.getElementById('compare-pokemon-name');
+  var pic = document.getElementById('compare-pokemon-pic');
+
+  name.innerText = capitalize(pokemonData.name);
+  pic.alt = capitalize(pokemonData.name);
+  pic.src = pokemonData.sprites.front_default;
+
+
+  var type = document.getElementById('compare-pokemon-type');
+
+  type.innerText = "Type: ";
+  pokemonData.types.forEach(function(element) {
+    type.innerText += element.type.name + '/'
+  });
+
+  var moveList = document.getElementById('compare-move-list');
+  removeChildNodes(moveList);
+
+  pokemonData.moves.forEach(function(attack) {
+    var li = document.createElement('li');
+    li.innerText = capitalize(attack.move.name);
+    moveList.appendChild(li);
+  });
+
+  comparePokemonStats(pokemonData);
+};
+
+
+var barSeries1 = [];
+var barCategories = [];
 
 var pokemonStats = function(pokemonData) {
   var container = document.getElementById('power-pie-chart');
-  var table = document.getElementById('stat-table');
+  var tableBody = document.getElementById('stat-table-body');
 
   var dataName = "Base Stats"
   var dataArr = [];
@@ -100,25 +159,14 @@ var pokemonStats = function(pokemonData) {
   var totalPower = 0;
 
 
-  removeChildNodes(table);
+  removeChildNodes(tableBody);
 
-  var topRow = document.createElement('tr');
-  var secondRow = document.createElement('tr');
-
-  var tableHead = document.createElement('th');
-  var secondRowType = document.createElement('th');
-  var secondRowStat = document.createElement('th');
-
-  secondRow.appendChild(secondRowType);
-  secondRow.appendChild(secondRowStat);
-  topRow.appendChild(tableHead);
+  var tableHead = document.getElementById('pokemon-table-heading');
 
   tableHead.innerText = title;
-  secondRowType.innerText = "Stat type";
-  secondRowStat.innerText = "Base Stat";
 
-  table.appendChild(topRow);
-  table.appendChild(secondRow);
+  barSeries1 = [];
+  barCategories = [];
 
   pokemonData.stats.forEach(function(segment) {
 
@@ -135,9 +183,15 @@ var pokemonStats = function(pokemonData) {
     statRowData.innerText = statName;
     valueRowData.innerText = baseStat;
 
+    barCategories.push(statName);
+    barSeries1.push(parseInt(baseStat));
+
+    statRowData.class = "stat-type"
+    valueRowData.class = "stat-value"
+
     row.appendChild(statRowData);
     row.appendChild(valueRowData);
-    table.appendChild(row);
+    tableBody.appendChild(row);
   });
 
   var totalStatRow = document.createElement('tr');
@@ -147,14 +201,124 @@ var pokemonStats = function(pokemonData) {
   totalStatData.innerText = totalPower;
   totalStatRow.appendChild(statRowText);
   totalStatRow.appendChild(totalStatData);
-  table.appendChild(totalStatRow);
+  tableBody.appendChild(totalStatRow);
 
   var output = [{name: dataName, data: dataArr}]
 
   var pieChart = new PieChart(container, title, output);
 
-  var power = document.getElementById('total-base-stats');
+  compareBarChart();
 };
+
+
+var barSeries2 = [];
+
+var comparePokemonStats = function(pokemonData) {
+  var container = document.getElementById('compare-power-pie-chart');
+  var tableBody = document.getElementById('compare-stat-table-body');
+
+  var dataName = "Base Stats"
+  var dataArr = [];
+  var title = `${capitalize(pokemonData.name)}\'s Base Stat Values`;
+  var totalPower = 0;
+
+
+  removeChildNodes(tableBody);
+
+  var tableHead = document.getElementById('compare-pokemon-table-heading');
+
+  tableHead.innerText = title;
+
+  barSeries2 = []
+
+  pokemonData.stats.forEach(function(segment) {
+
+    var statName = capitalize(segment.stat.name);
+    var baseStat = segment.base_stat;
+    var pokeData = {name: capitalize(statName), y: baseStat};
+    totalPower += parseInt(baseStat);
+    dataArr.push(pokeData);
+
+    var row = document.createElement('tr');
+    var statRowData = document.createElement('td');
+    var valueRowData = document.createElement('td');
+
+    statRowData.innerText = statName;
+    valueRowData.innerText = baseStat;
+
+    statRowData.class = "compare-stat-type"
+    valueRowData.class = "compare-stat-value"
+
+    barSeries2.push(parseInt(baseStat));
+
+    row.appendChild(statRowData);
+    row.appendChild(valueRowData);
+    tableBody.appendChild(row);
+  });
+
+  var totalStatRow = document.createElement('tr');
+  var statRowText = document.createElement('td');
+  var totalStatData = document.createElement('td');
+  statRowText.innerText = "Total Base Stat";
+  totalStatData.innerText = totalPower;
+  totalStatRow.appendChild(statRowText);
+  totalStatRow.appendChild(totalStatData);
+  tableBody.appendChild(totalStatRow);
+
+  var output = [{name: dataName, data: dataArr}]
+
+  var pieChart = new PieChart(container, title, output);
+
+  compareBarChart();
+
+}
+
+
+var compareBarChart = function() {
+  var container = document.getElementById('bar-chart');
+
+  var pokemon = document.getElementById('pokemon-name').innerText;
+  var comparePokemon = document.getElementById('compare-pokemon-name').innerText;
+  var title = `${pokemon} vs ${comparePokemon}`;
+
+  var categories = document.getElementsByClassName('stat-type');
+
+  // var pokemonStat = document.getElementById('stat-table-body');
+  // var comparePokemonStat = document.getElementById('compare-stat-table-body');
+  // var pokemonSeriesArr = document.getElementsByClassName('stat-value');
+  //
+  // var comparePokemonSeriesArr = document.getElementsByClassName('compare-stat-value');
+  //
+  //
+  //
+  // pokemonSeriesArr.forEach(function(value) {
+  //   var stat = parseInt(value);
+  //   series1.push(stat);
+  // });
+  //
+  //
+  // comparePokemonSeriesArr.forEach(function(value) {
+  //   var stat = parseInt(value);
+  //   series2.push(stat);
+  // })
+
+  var pokemonSeries = {
+    name: pokemon,
+    data: barSeries1,
+    color: "blue"
+  };
+
+  var comparePokemonSeries = {
+    name: comparePokemon,
+    data: barSeries2,
+    color: "red"
+  };
+
+  var series = [pokemonSeries, comparePokemonSeries];
+
+  var barChart = new BarChart(container, title, barCategories, series);
+
+}
 
 var removeChildNodes = function(node){
   while (node.hasChildNodes()) {
--- public/barChart.js
+++ public/barChart.js
@@ -1,3 +1,30 @@
+// var BarChart = function() {
+//   var container = document.querySelector("#bar-chart");
+//   var chart = new Highcharts.Chart({
+//     chart: {
+//       type: "column",
+//       renderTo: container
+//     },
+//     title: {
+//       text: "G3's favourite Science Fiction Franchises"
+//     },
+//     xAxis: {
+//       categories: ["Star Wars", "Star Trek", "BSG", "I hate nerds", "Jamie"]
+//     },
+//     series: [
+//       {
+//       name: "G3",
+//       data: [8, 2, 3, 3, 1],
+//       color: "red"
+//     },
+//     {
+//       name: "G4",
+//       data: [4, 4, 3, 3, 3],
+//       color: "black"
+//     }]
+//   });
+// };
+
 var BarChart = function(container, titleText, categoriesArr, seriesArr) {
   var chart = new Highcharts.Chart({
     chart: {
